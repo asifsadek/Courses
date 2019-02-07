dd8943da2e2df8639bc379d451de0afabbc9e561
Dilawar Singh <dilawars@ncbs.res.in>
2019-02-07 22:31:44 +0530

    Added frisbee  problem.

---
+++ TASHIP/InformationTheory2019/Tutorial1/frisbee.py
@@ -0,0 +1,21 @@
+"""frisbee.py: 
+
+"""
+    
+__author__           = "Dilawar Singh"
+__copyright__        = "Copyright 2017-, Dilawar Singh"
+__version__          = "1.0.0"
+__maintainer__       = "Dilawar Singh"
+__email__            = "dilawars@ncbs.res.in"
+__status__           = "Development"
+
+import sys
+import os
+import numpy as np
+import random
+import opencv as cv
+
+def main():
+
+if __name__ == '__main__':
+    main()
