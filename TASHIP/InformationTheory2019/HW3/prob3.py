a6130a816a87c4f952269ca12c85686d58e1f688
Dilawar Singh <dilawars@ncbs.res.in>
2019-03-03 23:22:32 +0530

    Rest do it on the campus.

--- TASHIP/InformationTheory2019/HW3/prob3.py
+++ TASHIP/InformationTheory2019/HW3/prob3.py
@@ -12,7 +12,7 @@ import numpy as np
 import math
 
 def ternary_trans(a, b, c):
-    x, y = b+c/2, 3*c/2
+    x, y = a/2+c, (3**0.5)*c/2
     return x, y
 
 def plot(data):
@@ -21,49 +21,43 @@ def plot(data):
     import pandas as pd
     import matplotlib.pyplot as plt
     a, b, c, h = [np.array(x) for x in zip(*data)]
-    x, y = a, b
-    X, Y = np.meshgrid(np.linspace(min(x), max(x), 200)
-            , np.linspace(min(y), max(y), 200)
-            )
-    Z = scipy.interpolate.griddata((x,y), h, (X, Y))
-    ax = plt.subplot(111)
-    c = ax.contour(X, Y, Z, levels=20, cmap='jet')
-    levels = c.levels
-    pts=[]
-    for i, segs in enumerate(c.allsegs):
-        z = levels[i]
-        for seg in segs:
-            for x, y in seg:
-                pts.append((x,y,1-x-y,z))
-
-    # now plot.
-    a, b, c, z = [np.array(x) for x in zip(*pts)]
     x, y = ternary_trans(a, b, c)
-    ax.clear()
-    im = plt.scatter(x, y, c=z, s=1, cmap='jet')
+    X, Y = np.meshgrid( np.linspace(min(x), max(x), 300)
+            , np.linspace(min(y), max(y), 300)
+            )
+    H = scipy.interpolate.griddata((x,y), h, (X, Y))
+    ax = plt.subplot(111)
+    im = ax.contour(X, Y, H, levels=20, cmap='jet')
+
+    pts = []
+    for i, l in enumerate(im.levels):
+        for segs in im.allsegs[i]:
+            for x, y in segs:
+                pts.append((x, y, l))
     plt.colorbar(im, ax=ax)
-    plt.title( '$H(a,b,c)$ where $a+b+c=1$')
     plt.savefig( f'{__file__}.png' )
     return pts
     
 
 def main():
     print( "[INFO ] Solving problem 3" )
-    dp = 3e-3
-    aa = np.arange(0, 1, dp)
+    dp = 5e-3
     ps = []
     allData = []
-    for a in aa:
+    for a in np.arange(0, 1, dp):
         for b in np.arange(0, 1-a, dp):
-            for c in np.arange(0, 1-a-b, dp/2):
+            for c in np.arange(0, 1-a-b, dp):
                 h = a*math.log(2,2)+b*math.log(3,2)+c*math.log(6,2)
                 allData.append((a,b,c,h))
     # plot the data.
     pts = plot(allData)
     with open('prob3.csv', 'w' ) as f:
         f.write( 'a b c h\n')
-        for x, y, z, h in pts:
-            f.write(f'{x} {y} {z} {h}\n')
+        for x, y, h in pts:
+            a = 2*(x-2*y/(3**0.5))
+            c = 2 * y / (3**0.5)
+            b = 1-a-c
+            f.write(f'{a} {b} {c} {h}\n')
     print( 'Done' )
     
 if __name__ == '__main__':
